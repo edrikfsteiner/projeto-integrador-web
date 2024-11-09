73b228035ee5ad342432e616c78fb940f2ec5457
edrikfsteiner <edrikfsteiner@gmail.com>
2024-11-09 15:14:32 -0300

    Update Client

--- frontend/src/Clientes/ClientSystem.js
+++ frontend/src/Clientes/ClientSystem.js
@@ -24,6 +24,8 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
+  maxHeight: '80vh',
+  overflowY: 'auto',
   bgcolor: 'background.paper',
   borderRadius: 2,
   boxShadow: 24,
@@ -36,7 +38,18 @@ function ClientSystem({ clients, setClients }) {
   const [detailsModalOpen, setDetailsModalOpen] = useState(false);
   const [clientToEdit, setClientToEdit] = useState(null);
   const [clientDetails, setClientDetails] = useState(null);
-  const [newClient, setNewClient] = useState({ name: '', email: '', phone: '' });
+  const [searchTerm, setSearchTerm] = useState("");
+  const [newClient, setNewClient] = useState({name: '',
+                                              email: '',
+                                              phone: '',
+                                              cpf: '',
+                                              birthdate: '',
+                                              number: '',
+                                              cep: '',
+                                              street: '',
+                                              district: '',
+                                              city: '',
+                                              uf: '' });
   const [editClient, setEditClient] = useState({});
 
   const handleOpen = () => setOpen(true);
@@ -59,12 +72,23 @@ function ClientSystem({ clients, setClients }) {
   const handleCloseDetailsModal = () => setDetailsModalOpen(false);
 
   const handleAddClient = () => {
-    if (newClient.name && newClient.email && newClient.phone) {
-      setClients([...clients, newClient]);
-      setNewClient({ name: '', email: '', phone: '' });
+    if (newClient.name && newClient.email && newClient.phone && isValidCpf(newClient.cpf) && isValidBirthdate(newClient.birthdate)) {
+      const formattedCpf = formatCpf(newClient.cpf);
+      setClients([...clients, { ...newClient, cpf: formattedCpf }]);
+      setNewClient({name: '',
+                    email: '',
+                    phone: '',
+                    cpf: '',
+                    birthdate: '',
+                    number: '',
+                    cep: '',
+                    street: '',
+                    district: '',
+                    city: '',
+                    uf: '' });
       handleClose();
     } else {
-      alert("Por favor, preencha todos os campos.");
+      alert("Por favor, preencha todos os campos corretamente.");
     }
   };
 
@@ -80,6 +104,10 @@ function ClientSystem({ clients, setClients }) {
     handleCloseEditModal();
   };
 
+  const filteredClients = clients.filter(client =>
+    client.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   const isValidText = (value) => /^[A-Za-záéíóúàèìòùâêîôûãõçÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕÇ\s]+$/.test(value);
 
   const isValidEmail = (email) => {
@@ -87,10 +115,20 @@ function ClientSystem({ clients, setClients }) {
     return regex.test(email);
   };
 
+  const isValidBirthdate = (date) => {
+    const birthDate = new Date(date);
+    const today = new Date();
+    return birthDate < today;
+  };  
+
+  const isValidCpf = (cpf) => /^\d{11}$/.test(cpf);
+
+  const formatCpf = (cpf) => cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+
   const formatPhone = (value) => {
     const onlyNumbers = value.replace(/\D/g, '');
-    if (onlyNumbers.length === 10) {
-      return `(${onlyNumbers.substring(0, 2)}) ${onlyNumbers.substring(2, 7)}-${onlyNumbers.substring(7, 11)}`;
+    if (onlyNumbers.length === 11) {
+      return `(${onlyNumbers.substring(0, 2)}) ${onlyNumbers.substring(2, 7)}-${onlyNumbers.substring(7, 12)}`;
     }
     return onlyNumbers;
 };
@@ -101,12 +139,21 @@ function ClientSystem({ clients, setClients }) {
         Sistema de Clientes
       </Typography>
 
+      <TextField
+        label="Pesquisar Cliente"
+        variant="outlined"
+        fullWidth
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        sx={{ mb: 2 }}
+      />
+
       <Paper elevation={3} sx={{ p: 2, mb: 3 }}>
         <Typography variant="h6" gutterBottom>
           Lista de Clientes
         </Typography>
         <List>
-          {clients.map((client, index) => (
+          {filteredClients.map((client, index) => (
             <ListItem
               key={index}
               secondaryAction={
@@ -127,7 +174,16 @@ function ClientSystem({ clients, setClients }) {
               </ListItemAvatar>
               <ListItemText
                 primary={client.name}
-                secondary={`Email: ${client.email} | Celular: ${client.phone}`}
+                secondary={`Email: ${client.email} | 
+                            Celular: ${formatPhone(client.phone)} | 
+                            CPF: ${client.cpf} | 
+                            Data de nascimento: ${client.birthdate} | 
+                            Número: ${client.number} | 
+                            CEP: ${client.cep} | 
+                            Rua: ${client.street} | 
+                            Bairro: ${client.district} | 
+                            Cidade: ${client.city} | 
+                            UF: ${client.uf}`}
               />
             </ListItem>
           ))}
@@ -155,6 +211,7 @@ function ClientSystem({ clients, setClients }) {
                 setNewClient({ ...newClient, name: e.target.value })
               }
             }}
+            inputProps={{ maxLength: 100 }}
             sx={{ mb: 2 }}
             required
           />
@@ -164,6 +221,7 @@ function ClientSystem({ clients, setClients }) {
             type="email"
             value={newClient.email}
             onChange={(e) => setNewClient({ ...newClient, email: e.target.value })}
+            inputProps={{ maxLength: 100 }}
             sx={{ mb: 2 }}
             required
           />
@@ -173,9 +231,102 @@ function ClientSystem({ clients, setClients }) {
             value={formatPhone(newClient.phone)}
             onChange={(e) => {
               const value = e.target.value;
-              const numericValue = value.replace(/\D/g, '').slice(0, 10); 
+              const numericValue = value.replace(/\D/g, '').slice(0, 11); 
               setNewClient({ ...newClient, phone: numericValue });
             }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="CPF"
+            value={formatCpf(newClient.cpf)}
+            onChange={(e) => {
+              const cpfValue = e.target.value.replace(/\D/g, '').slice(0, 11);
+              setNewClient({ ...newClient, cpf: cpfValue });
+            }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Data de Nascimento"
+            type="date"
+            value={newClient.birthdate}
+            onChange={(e) => setNewClient({ ...newClient, birthdate: e.target.value })}
+            InputLabelProps={{ shrink: true }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Número"
+            value={newClient.number}
+            onChange={(e) => setNewClient({ ...newClient, number: e.target.value })}
+            inputProps={{ maxLength: 10 }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="CEP"
+            value={newClient.cep}
+            onChange={(e) => {
+              const numericValue = e.target.value.replace(/\D/g, '').slice(0, 8);
+              const formattedValue = numericValue.replace(/(\d{5})(\d{3})/, '$1-$2');
+              setNewClient({ ...newClient, cep: formattedValue });
+            }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Rua"
+            value={newClient.street}
+            onChange={(e) => setNewClient({ ...newClient, street: e.target.value })}
+            inputProps={{ maxLength: 200 }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Bairro"
+            value={newClient.district}
+            onChange={(e) => {
+              const value = e.target.value
+              if (isValidText(value) || value === "") {
+                setNewClient({ ...newClient, district: e.target.value })
+              }
+            }}
+            inputProps={{ maxLength: 80 }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Cidade"
+            value={newClient.city}
+            onChange={(e) => {
+              const value = e.target.value
+              if (isValidText(value) || value === "") {
+                setNewClient({ ...newClient, city: e.target.value })
+              }
+            }}
+            inputProps={{ maxLength: 80 }}
+            sx={{ mb: 2 }}
+            required
+          />
+          <TextField
+            fullWidth
+            label="UF"
+            value={newClient.uf}
+            onChange={(e) => {
+              const value = e.target.value
+              if (isValidText(value) || value === "") {
+                setNewClient({ ...newClient, uf: e.target.value })
+              }
+            }}
+            inputProps={{ maxLength: 2 }}
             sx={{ mb: 2 }}
             required
           />
@@ -184,11 +335,20 @@ function ClientSystem({ clients, setClients }) {
             fullWidth
             onClick={handleAddClient}
             disabled={
-              !newClient.name || 
-              !newClient.email || 
-              !newClient.phone || 
+              !newClient.name ||
+              !newClient.email ||
               !isValidEmail(newClient.email) ||
-              newClient.phone.length !== 10
+              !newClient.phone || 
+              newClient.phone.length !== 11 ||
+              !newClient.cpf || 
+              !isValidCpf(newClient.cpf) ||
+              !newClient.birthdate ||
+              !newClient.number ||
+              !newClient.cep ||
+              !newClient.street ||
+              !newClient.district ||
+              !newClient.city ||
+              !newClient.uf
             }
           >
             Adicionar
@@ -203,22 +363,11 @@ function ClientSystem({ clients, setClients }) {
           </Typography>
           <TextField
             fullWidth
-            label="Nome"
-            value={editClient.name || ''}
-            onChange={(e) => {
-              const value = e.target.value
-              if (isValidText(value) || value === "") {
-                setEditClient({ ...editClient, name: e.target.value })
-              }
-            }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
             label="Email"
             type="email"
             value={editClient.email || ''}
             onChange={(e) => setEditClient({ ...editClient, email: e.target.value })}
+            inputProps={{ maxLength: 100 }}
             sx={{ mb: 2 }}
           />
           <TextField
@@ -227,9 +376,75 @@ function ClientSystem({ clients, setClients }) {
             value={formatPhone(editClient.phone || '')}
             onChange={(e) => {
               const value = e.target.value;
-              const numericValue = value.replace(/\D/g, '').slice(0, 10); 
+              const numericValue = value.replace(/\D/g, '').slice(0, 11); 
               setEditClient({ ...editClient, phone: numericValue });
             }}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="Número"
+            value={editClient.number}
+            onChange={(e) => setEditClient({ ...editClient, number: e.target.value })}
+            inputProps={{ maxLength: 10 }}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="CEP"
+            value={editClient.cep}
+            onChange={(e) => {
+              const numericValue = e.target.value.replace(/\D/g, '').slice(0, 8);
+              const formattedValue = numericValue.replace(/(\d{5})(\d{3})/, '$1-$2');
+              setEditClient({ ...editClient, cep: formattedValue });
+            }}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="Rua"
+            value={editClient.street}
+            onChange={(e) => setEditClient({ ...editClient, street: e.target.value })}
+            inputProps={{ maxLength: 200 }}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="Bairro"
+            value={editClient.district}
+            onChange={(e) => {
+              const value = e.target.value
+              if (isValidText(value) || value === "") {
+                setEditClient({ ...editClient, district: e.target.value })
+              }
+            }}
+            inputProps={{ maxLength: 80 }}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="Cidade"
+            value={editClient.city}
+            onChange={(e) => {
+              const value = e.target.value
+              if (isValidText(value) || value === "") {
+                setEditClient({ ...editClient, city: e.target.value })
+              }
+            }}
+            inputProps={{ maxLength: 80 }}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="UF"
+            value={editClient.uf}
+            onChange={(e) => {
+              const value = e.target.value
+              if (isValidText(value) || value === "") {
+                setEditClient({ ...editClient, uf: e.target.value })
+              }
+            }}
+            inputProps={{ maxLength: 2 }}
             sx={{ mb: 2 }}
           />
           <Button
@@ -237,11 +452,16 @@ function ClientSystem({ clients, setClients }) {
             fullWidth
             onClick={handleSaveEdit}
             disabled={
-              !editClient.name || 
-              !editClient.email || 
-              !editClient.phone || 
+              !editClient.email ||
               !isValidEmail(editClient.email) ||
-              editClient.phone.length !== 10
+              !editClient.phone || 
+              editClient.phone.length !== 11 ||
+              !editClient.number ||
+              !editClient.cep ||
+              !editClient.street ||
+              !editClient.district ||
+              !editClient.city ||
+              !editClient.uf
             }
           >
             Salvar
@@ -278,6 +498,78 @@ function ClientSystem({ clients, setClients }) {
                 fullWidth
                 label="Celular"
                 value={clientDetails.phone}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="CPF"
+                value={clientDetails.cpf}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="Data de nascimento"
+                value={clientDetails.birthdate}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="Número"
+                value={clientDetails.number}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="CEP"
+                value={clientDetails.cep}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="Rua"
+                value={clientDetails.street}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="Bairro"
+                value={clientDetails.district}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="Cidade"
+                value={clientDetails.cidade}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                label="UF"
+                value={clientDetails.uf}
                 InputProps={{
                   readOnly: true,
                 }}
